17aa8e24bc6d7db53a9f0baf1d57899b4d7d6b30
Brian Corey <briancorey87@gmail.com>
2025-03-09 20:48:21 -0400

    add support for SVG

--- functions.php
+++ functions.php
@@ -22,3 +22,11 @@ endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
 // END ENQUEUE PARENT ACTION
+
+function add_file_types_to_uploads($file_types){
+    $new_filetypes = array();
+    $new_filetypes['svg'] = 'image/svg+xml';
+    $file_types = array_merge($file_types, $new_filetypes );
+    return $file_types;
+}
+add_filter('upload_mimes', 'add_file_types_to_uploads');
